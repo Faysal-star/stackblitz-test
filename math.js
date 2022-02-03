0ee8a037108fae0e6345428c70d94bad20d13d90
Faysal-star <kishorepasha347@gmail.com>
2022-02-03 21:29:30 +0600

    str modified

--- math.js
+++ math.js
@@ -25,14 +25,66 @@ console.log(max2); */
 
 // All number add in array
 
-function sum(a) {
-  var sum = 0;
-  for (var i = 0; i < a.length; i++) {
-    sum = sum + a[i];
+// function sum(a) {
+//   var sum = 0;
+//   for (var i = 0; i < a.length; i++) {
+//     sum = sum + a[i];
+//   }
+//   return sum;
+// }
+
+// var arr = [2, 3, 4, 5, 6, 12, 13, -1, 0];
+
+// console.log(sum(arr));
+
+// Remove Duplicate
+
+/* function removedup(arr) {
+  // var i;
+  // var j;
+  // for (i = 0; i < arr.length; i++) {
+  //   for (j = i + 1; j < arr.length; j++) {
+  //     if (arr[i] == arr[j]) {
+  //       arr.splice(j, 1);
+  //     }
+  //   }
+  // }
+  const uniq = [];
+  for (const element of arr) {
+    if (uniq.indexOf(element) == -1) {
+      uniq.push(element);
+    }
   }
-  return sum;
+  return uniq;
 }
 
-var arr = [2, 3, 4, 5, 6, 12, 13, -1, 0];
+var arr = [
+  'ami',
+  'tumi',
+  'se',
+  'ami',
+  'tumi',
+  'se',
+  'nai',
+  'khai',
+  'tumi',
+  'je',
+];
+console.log(removedup(arr));
+*/
 
-console.log(sum(arr));
+// Reverse String
+/* function rev(str) {
+  let str2 = '';
+  // for (let j = str.length - 1; j >= 0; j--) {
+  //   str2 = str2 + str[j];
+  // }
+  for (const letter of str) {
+    str2 = letter + str2;
+  }
+  return str2;
+}
+
+let str = 'hola amigoes';
+console.log(rev(str));
+*/
